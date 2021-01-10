a769f7b3cd8012ee27bf9e92e827675620b593ba
IsmailQamber <ism-you-95@hotmail.com>
2021-01-10 15:20:54 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,17 @@
 // Define the numbers and the operation here
+const FirstNumber = 3;
+const SecondNumber = 5;
+const operator = window.prompt("enter your operations: ");
 
 // Write if-statements here
+
+
+if(operator === "+"){
+    console.log(FirstNumber + SecondNumber)
+} else if (operator === "-") {
+    console.log(FirstNumber - SecondNumber)
+} else if (operator === "/"){
+  console.log(FirstNumber / SecondNumber)
+} else {
+    console.log("This operation is not available")
+}
